b68f6b2ca0440e3b0f7d0b7abbb6d5074162175b
ARTHURpvn <arthursppavan@gmail.com>
2024-11-10 22:46:16 -0300

    feat: add ai report generation

---
+++ app/(home)/_components/ai-report-button.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { Button } from "@/app/_components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/app/_components/ui/dialog";
+import { DialogClose } from "@radix-ui/react-dialog";
+import { BotIcon, Loader2Icon } from "lucide-react";
+import { generateAiReport } from "../_actions/generate-ai-report";
+import { useState } from "react";
+import { ScrollArea } from "@/app/_components/ui/scroll-area";
+import Markdown from "react-markdown";
+
+interface AIReportButtonProps {
+  month: string;
+}
+
+const AiReportButton = ({ month }: AIReportButtonProps) => {
+  const [report, setReport] = useState<string | null>();
+  const [reportIsLoading, setReportIsLoading] = useState(false);
+  const handleGenerateReportClick = async () => {
+    try {
+      setReportIsLoading(true);
+      const aiReport = await generateAiReport({ month });
+      setReport(aiReport);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setReportIsLoading(false);
+    }
+  };
+  return (
+    <Dialog
+      onOpenChange={(open) => {
+        if (!open) {
+          setReport(null);
+        }
+      }}
+    >
+      <DialogTrigger asChild>
+        <Button variant={"ghost"}>
+          Relatorio AI
+          <BotIcon />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="max-w-[600px]">
+        <DialogHeader>
+          <DialogTitle>Relatorio AI</DialogTitle>
+          <DialogDescription>
+            Use a Inteligência artificial para gerar um relatório com insights
+            sobre suas finanças
+          </DialogDescription>
+        </DialogHeader>
+        <ScrollArea className="prose prose-h3:text-white prose-h4:text-white prose-strong:text-white max-h-[450px] text-white">
+          <Markdown>{report}</Markdown>
+        </ScrollArea>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button variant={"ghost"}>Cancelar</Button>
+          </DialogClose>
+          <Button
+            onClick={handleGenerateReportClick}
+            disabled={reportIsLoading}
+          >
+            {reportIsLoading && <Loader2Icon className="animate-spin" />}
+            Gerar Relatório
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default AiReportButton;
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -9,6 +9,7 @@ import { getDashboard } from "../_data/get-dashboard";
 import ExpensesPerCateory from "./_components/expenses-per-cateory";
 import LastTransacation from "./_components/last-transacation";
 import { canUserAddTransaction } from "../_data/can-user-add-transaction";
+import AiReportButton from "./_components/ai-report-button";
 
 interface Props {
   searchParams: {
@@ -35,7 +36,10 @@ const Home = async ({ searchParams: { month } }: Props) => {
       <div className="space-y-6 p-6 flex flex-col overflow-hidden">
         <div className="flex justify-between">
           <h1 className="text-2xl font-bold"> Dashboard </h1>
-          <TimeSelect />
+          <div className="flex items-center gap-3">
+            <AiReportButton month={month} />
+            <TimeSelect />
+          </div>
         </div>
         <div className="grid grid-cols-[2fr,1fr] gap-6 overflow-hidden">
           <div className="flex flex-col gap-6 overflow-hidden">
